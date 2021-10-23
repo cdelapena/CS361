78119298600852de12a62caa5f7c670ded8c5d86
cdelapena <delapc@gmail.com>
2021-10-22 23:50:24 -0700

    incorporated API into method to obtain distance between cities.

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
+import requests
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
+
+def get_distance(startingcity, destinationcity):
+    url = f'https://www.distance24.org/route.json?stops={startingcity}|{destinationcity}'
+    response = requests.get(url).json()
+    return response
 
 @app.route('/')
 def index():
@@ -10,9 +16,23 @@ def index():
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-    return render_template('result.html', startingcity = request.form['starting-city'], 
-    destinationcity = request.form['destination-city'],
-    priority = request.form['gridRadios'])
+    startingcity = request.form['starting-city']
+    destinationcity = request.form['destination-city']
+    priority = request.form['gridRadios']
+
+    distance = get_distance(startingcity, destinationcity)
+    km = distance["distance"]
+    miles = int(km * 0.621371)
+
+    # check min range
+    # check if same city
+
+    return render_template('result.html', distance=distance, miles=miles, startingcity=startingcity, destinationcity=destinationcity, priority=priority)
+    #vars = render_template('result.html', startingcity = request.form['starting-city'], 
+    #destinationcity = request.form['destination-city'],
+    #priority = request.form['gridRadios'])
+
+    #return vars
 
 if __name__ == '__main__' :
     app.run(debug=True)
