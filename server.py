ac8d4b5c6e88ddc33c4a9af5de5d0166ef2a4c4c
cdelapena <delapc@gmail.com>
2021-10-23 22:52:33 -0700

    implemented beautiful soup for scraping

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import requests
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from bs4 import BeautifulSoup
+from urllib.request import urlopen, Request, urlretrieve
+import urllib
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -10,8 +13,22 @@ def get_distance(startingcity, destinationcity):
     response = requests.get(url).json()
     return response
 
+def scrape_plane(airplane):
+    url = f'https://www.airlines-inform.com/commercial-aircraft/{airplane}.html'
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    picture = soup.find(class_="setWOfMe")
+ 
+    return picture['src']
+
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/reset', methods=['GET'])
+def reset():
     return render_template('index.html')
 
 @app.route('/result', methods=['GET', 'POST'])
@@ -19,6 +36,16 @@ def result():
     startingcity = request.form['starting-city']
     destinationcity = request.form['destination-city']
     priority = request.form['gridRadios']
+    airplane_names = {"name" : ['airbus-a320neo', 'airbus-a321neo', 'airbus-a330-300', 'boeing-737-max-10', 'boeing-787-10', 
+        'bombardier-cs100', 'bombardier-cs300', 'embraer-e195-e2']}
+
+    # build dictionary of dictionaries using scraper
+    airplane_details = {}
+    for i in range(0, len(airplane_names["name"])): 
+        # Create dictionary or will get keyerror0
+        airplane_details[i] = {}
+        airplane_details[i]["name"] = airplane_names["name"][i]
+        airplane_details[i]["picture"] = scrape_plane(airplane_names["name"][i])
 
     distance = get_distance(startingcity, destinationcity)
     km = distance["distance"]
@@ -27,7 +54,8 @@ def result():
     # check min range
     # check if same city
 
-    return render_template('result.html', distance=distance, miles=miles, startingcity=startingcity, destinationcity=destinationcity, priority=priority)
+    return render_template('result.html', distance=distance, miles=miles, startingcity=startingcity, 
+    destinationcity=destinationcity, priority=priority, airplane_details=airplane_details)
     #vars = render_template('result.html', startingcity = request.form['starting-city'], 
     #destinationcity = request.form['destination-city'],
     #priority = request.form['gridRadios'])
